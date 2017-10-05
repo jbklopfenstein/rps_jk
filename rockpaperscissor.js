ded1f742f092da02364c538499d4d55e1d00f7b5
Jeff Klopfenstein <jbklopfenstein@gmail.com>
2017-10-04 21:12:57 -0400

    tried, but keep getting an infinite loop :( too tired now...

--- rockpaperscissor.js
+++ rockpaperscissor.js
@@ -1,21 +1,60 @@
-var player1 = "mickey";
-var player2 = "goofy";
 var mickeyscore = 0;
 var goofyscore = 0;
-
 var weapons = ['rock', 'paper', 'scissors'];
-var mickeyweaponOfChoice = weapons[parseInt(Math.random()*weapons.length) %3];
-var goofyweaponOfChoice = weapons[parseInt(Math.random()*weapons.length) %2];
-console.log(player1 + " played " + mickeyweaponOfChoice);
-console.log(player2 + " played " + goofyweaponOfChoice);
 
-//while (mickeyscore || goofyscore < 3) {
+while (mickeyscore || goofyscore < 3) {
+  var mickeyweaponOfChoice = weapons[parseInt(Math.random()*weapons.length) %3];
+  var goofyweaponOfChoice = weapons[parseInt(Math.random()*weapons.length) %2];
+  console.log("mickey played " + mickeyweaponOfChoice);
+  console.log("goofy played " + goofyweaponOfChoice);
+
+//tie scenarios
   if(mickeyweaponOfChoice == 'rock' && goofyweaponOfChoice == 'rock'){
      console.log('tie, go again');
   }
-  else if(mickeyweaponOfChoice == 'rock' && goofyweaponOfChoice == 'scissors') {
-     goofyscore + 1;
-     console.log(goofyscore);
+  else if(mickeyweaponOfChoice == 'paper' && goofyweaponOfChoice == 'paper'){
+     console.log('tie, go again');
   }
+  else if(mickeyweaponOfChoice == 'scissors' && goofyweaponOfChoice == 'scissors'){
+     console.log('tie, go again');
+  }
+//mickey hand wins
+  else if(mickeyweaponOfChoice == 'rock' && goofyweaponOfChoice == 'scissors'){
+     mickeyscore = mickeyscore + 1;
+     console.log("mickey has " + mickeyscore);
+     console.log("...");
+  }
+  else if(mickeyweaponOfChoice == 'paper' && goofyweaponOfChoice == 'rock'){
+     mickeyscore = mickeyscore + 1;
+     console.log("mickey has " + mickeyscore);
+     console.log("...");
+  }
+  else if(mickeyweaponOfChoice == 'scissors' && goofyweaponOfChoice == 'paper'){
+     mickeyscore = mickeyscore + 1;
+     console.log("mickey has " + mickeyscore);
+     console.log("...");
+  }
+//goofy hand wins
+  else if(mickeyweaponOfChoice == 'scissors' && goofyweaponOfChoice == 'rock'){
+     goofyscore = goofyscore + 1;
+     console.log("goofy has " + goofyscore);
+     console.log("...");
+  }
+  else if(mickeyweaponOfChoice == 'rock' && goofyweaponOfChoice == 'paper'){
+     goofyscore = goofyscore + 1;
+     console.log("goofy has " + goofyscore);
+     console.log("...");
+  }
+  else(mickeyweaponOfChoice == 'paper' && goofyweaponOfChoice == 'scissors');{
+     goofyscore = goofyscore + 1;
+     console.log("goofy has " + goofyscore);
+     console.log("...");
+  }
+}
 
-//}
+if (mickeyscore == 3) {
+  console.log("mickey wins!");
+}
+else if (goofyscore == 3) {
+  console.log("goofy wins!");
+}
